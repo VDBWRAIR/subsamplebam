ad964aba4a9841d9137e7703add8cccac080f652
Tyghe Vallard <vallardt@gmail.com>
2015-01-13 09:26:11 -0500

    now installs into correct location

--- tests/install_samtools.sh
+++ tests/install_samtools.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -ev
 
+prefix=$PWD
 version=1.1
 git clone https://github.com/samtools/htslib.git
 git clone https://github.com/samtools/samtools.git
 cd samtools
 git checkout $version
-sed -i "s|prefix\s\+=\s\+/usr/local|prefix = $PWD|" Makefile
+sed -i "s|prefix\s\+=\s\+/usr/local|prefix = $prefix|" Makefile
 make
 make install
