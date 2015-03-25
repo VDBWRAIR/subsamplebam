3eb29f5ab2ff4bac9a7986c268bbc60d0f6163b3
Panciera <michael.panciera@amedwrair15370.amed.ds.army.mil>
2015-03-25 12:57:39 -0400

    deleted files

--- min.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-tmp="tmp2/minimized.780.depth200"
-ngs="/home/AMED/michael.panciera/projects/ngs_mapper/ngs_mapper"
-python subsample_mindepth.py $1 $2 $3 | samtools view -hSb - | samtools sort - $tmp; samtools index $tmp.bam;
-python $ngs/bam_to_qualdepth.py $tmp.bam > $tmp.json
-python  $ngs/graph_qualdepth.py $tmp.json -o $tmp.png
